6e1e3858373eb44e99515e38be77ea45a231cdbd
Jesse Lee <jqlee85@gmail.com>
2017-06-21 22:18:13 -0500

    update plugin name and desc

--- organic-widgets.php
+++ organic-widgets.php
@@ -1,21 +1,15 @@
 <?php
 
 /**
- * The plugin bootstrap file
- *
- * This file is read by WordPress to generate the plugin information in the plugin
- * admin area. This file also includes all of the dependencies used by the plugin,
- * registers the activation and deactivation functions, and defines a function
- * that starts the plugin.
  *
  * @link              https://organicthemes.com
  * @since             1.0.0
  * @package           Organic_Widgets
  *
  * @wordpress-plugin
- * Plugin Name:       Organic Widgets
+ * Plugin Name:       Organic Customizer Widgets
  * Plugin URI:        https://organicthemes.com
- * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
+ * Description:       Adds a page template with a new widget areas and widgets to a site for more display flexibility
  * Version:           1.0.0
  * Author:            Organic Themes
  * Author URI:        https://organicthemes.com
