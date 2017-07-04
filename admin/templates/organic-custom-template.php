a09d145ac63a365ad5a93a4c825acd4ec9a9c4f2
David Morgan <itsdavidmorgan@gmail.com>
2017-07-03 18:53:38 -1000

    Style and template tweaks

--- admin/templates/organic-custom-template.php
+++ admin/templates/organic-custom-template.php
@@ -15,22 +15,6 @@ if ( locate_template( 'organic-custom-template.php' ) != '' ) {
 	// Load the default Custom Template from the plugin
 	get_header(); ?>
 
-	<?php $thumb = ( '' != get_the_post_thumbnail() ) ? wp_get_attachment_image_src( get_post_thumbnail_id(), 'full' ) : false; ?>
-
-	<?php if ( has_post_thumbnail() ) { ?>
-
-	<!-- BEGIN .row -->
-	<div class="row">
-
-		<div class="feature-img banner-img" style="background-image: url(<?php echo esc_url( $thumb[0] ); ?>);">
-			<?php the_post_thumbnail(); ?>
-		</div>
-
-	<!-- END .row -->
-	</div>
-
-	<?php } ?>
-
 	<!-- BEGIN .post class -->
 	<div <?php post_class(); ?> id="page-<?php the_ID(); ?>">
 
