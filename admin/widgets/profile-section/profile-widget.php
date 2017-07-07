bcd26a08338cddc43dc5b6945678f4e44733d31e
Jesse Lee <jqlee85@gmail.com>
2017-07-06 19:36:06 -0700

    add group checking for widgets

--- admin/widgets/profile-section/profile-widget.php
+++ admin/widgets/profile-section/profile-widget.php
@@ -65,80 +65,90 @@ class Organic_Widgets_Profile_Widget extends Organic_Widgets_Custom_Widget {
 		$instance['facebook_url'] = isset( $instance['facebook_url'] ) ? $instance['facebook_url'] : false;
 		$instance['email'] = isset( $instance['email'] ) ? $instance['email'] : false;
 
-		echo $args['before_widget'];
+		if ( organic_widgets_is_first_groupable_widget( $args, $instance ) && ! organic_widgets_is_last_groupable_widget( $args, $instance ) ) {
+			$first_last = ' organic-widgets-groupable-first';
+		} elseif ( organic_widgets_is_last_groupable_widget( $args, $instance ) && ! organic_widgets_is_first_groupable_widget( $args, $instance ) ) {
+			$first_last = ' organic-widgets-groupable-last';
+		} else {
+			$first_last = false;
+		}
+
+
 		?>
+
+		<?php echo $args['before_widget']; ?>
 
 		<?php if ( '' != $title || '' != $text || '' != $subtitle ) { ?>
 
-			<!-- BEGIN .organic-widgets-section -->
-			<div class="organic-widgets-section organic_widgets-profile-section" <?php if ($instance['bg_color']) { ?>style="background-color:<?php echo $instance['bg_color']; ?>;"<?php } ?>>
+				<!-- BEGIN .organic-widgets-section -->
+				<div class="organic-widgets-section organic_widgets-profile-section<?php if ( $first_last ) { echo esc_attr( $first_last ); } ?>" <?php if ($instance['bg_color']) { ?>style="background-color:<?php echo $instance['bg_color']; ?>;"<?php } ?>>
 
-				<!-- BEGIN .organic-widgets-profile -->
-				<div class="organic-widgets-profile">
+					<!-- BEGIN .organic-widgets-profile -->
+					<div class="organic-widgets-profile">
 
-					<?php if ( $instance['bg_image_id'] > 0 ) { ?>
-						<div class="organic-widgets-profile-img"><img src="<?php echo $instance['bg_image']; ?>" alt="<?php __( 'Profile Image', ORGANIC_WIDGETS_18N ) ?>" /></div>
-					<?php } ?>
-
-					<!-- BEGIN .organic-profile-content -->
-					<div class="organic-profile-content">
-
-					<?php if ( ! empty( $title ) ) { ?>
-						<h6 class="organic-widgets-profile-title"><?php echo apply_filters( 'widget_title', $title ); ?></h6>
-					<?php } ?>
-
-					<?php if ( ! empty( $subtitle ) ) { ?>
-						<h3 class="organic-widgets-profile-sub-title"><?php echo $subtitle; ?></h3>
-					<?php } ?>
-
-						<div class="organic-widgets-profile-divider"></div>
-
-					<?php if ( ! empty( $text ) ) { ?>
-						<div class="organic-widgets-profile-excerpt"><?php echo $text; ?></div>
-					<?php } ?>
-
-					<?php if ( ! empty( $instance['personal_url'] ) || ! empty( $instance['twitter_url'] ) || ! empty( $instance['linkedin_url'] ) || ! empty( $instance['facebook_url'] ) || ! empty( $instance['email'] ) ) { ?>
-
-					<ul class="organic-widgets-social-icons">
-
-						<?php if ( ! empty( $instance['personal_url'] ) ) { ?>
-							<li><a href="<?php echo $instance['personal_url']; ?>" target="_blank"><span><?php esc_html_e( 'Personal Link', ORGANIC_WIDGETS_18N ); ?></span></a></li>
+						<?php if ( $instance['bg_image_id'] > 0 ) { ?>
+							<div class="organic-widgets-profile-img"><img src="<?php echo $instance['bg_image']; ?>" alt="<?php __( 'Profile Image', ORGANIC_WIDGETS_18N ) ?>" /></div>
 						<?php } ?>
 
-						<?php if ( ! empty( $instance['twitter_url'] ) ) { ?>
-							<li><a href="<?php echo $instance['twitter_url']; ?>" target="_blank"><span><?php esc_html_e( 'Twitter', ORGANIC_WIDGETS_18N ); ?></span></a></li>
+						<!-- BEGIN .organic-profile-content -->
+						<div class="organic-profile-content">
+
+						<?php if ( ! empty( $title ) ) { ?>
+							<h6 class="organic-widgets-profile-title"><?php echo apply_filters( 'widget_title', $title ); ?></h6>
 						<?php } ?>
 
-						<?php if ( ! empty( $instance['linkedin_url'] ) ) { ?>
-							<li><a href="<?php echo $instance['linkedin_url']; ?>" target="_blank"><span><?php esc_html_e( 'LinkedIn', ORGANIC_WIDGETS_18N ); ?></span></a></li>
+						<?php if ( ! empty( $subtitle ) ) { ?>
+							<h3 class="organic-widgets-profile-sub-title"><?php echo $subtitle; ?></h3>
 						<?php } ?>
 
-						<?php if ( ! empty( $instance['facebook_url'] ) ) { ?>
-							<li><a href="<?php echo $instance['facebook_url']; ?>" target="_blank"><span><?php esc_html_e( 'Facebook', ORGANIC_WIDGETS_18N ); ?></span></a></li>
+							<div class="organic-widgets-profile-divider"></div>
+
+						<?php if ( ! empty( $text ) ) { ?>
+							<div class="organic-widgets-profile-excerpt"><?php echo $text; ?></div>
 						<?php } ?>
 
-						<?php if ( ! empty( $instance['email'] ) ) { ?>
-							<li><a href="mailto:<?php echo $instance['email']; ?>" target="_blank"><span><?php esc_html_e( 'Email', ORGANIC_WIDGETS_18N ); ?></span></a></li>
+						<?php if ( ! empty( $instance['personal_url'] ) || ! empty( $instance['twitter_url'] ) || ! empty( $instance['linkedin_url'] ) || ! empty( $instance['facebook_url'] ) || ! empty( $instance['email'] ) ) { ?>
+
+						<ul class="organic-widgets-social-icons">
+
+							<?php if ( ! empty( $instance['personal_url'] ) ) { ?>
+								<li><a href="<?php echo $instance['personal_url']; ?>" target="_blank"><span><?php esc_html_e( 'Personal Link', ORGANIC_WIDGETS_18N ); ?></span></a></li>
+							<?php } ?>
+
+							<?php if ( ! empty( $instance['twitter_url'] ) ) { ?>
+								<li><a href="<?php echo $instance['twitter_url']; ?>" target="_blank"><span><?php esc_html_e( 'Twitter', ORGANIC_WIDGETS_18N ); ?></span></a></li>
+							<?php } ?>
+
+							<?php if ( ! empty( $instance['linkedin_url'] ) ) { ?>
+								<li><a href="<?php echo $instance['linkedin_url']; ?>" target="_blank"><span><?php esc_html_e( 'LinkedIn', ORGANIC_WIDGETS_18N ); ?></span></a></li>
+							<?php } ?>
+
+							<?php if ( ! empty( $instance['facebook_url'] ) ) { ?>
+								<li><a href="<?php echo $instance['facebook_url']; ?>" target="_blank"><span><?php esc_html_e( 'Facebook', ORGANIC_WIDGETS_18N ); ?></span></a></li>
+							<?php } ?>
+
+							<?php if ( ! empty( $instance['email'] ) ) { ?>
+								<li><a href="mailto:<?php echo $instance['email']; ?>" target="_blank"><span><?php esc_html_e( 'Email', ORGANIC_WIDGETS_18N ); ?></span></a></li>
+							<?php } ?>
+
+						</ul>
+
 						<?php } ?>
 
-					</ul>
+						<!-- END .organic-profile-content -->
+						</div>
 
-					<?php } ?>
-
-					<!-- END .organic-profile-content -->
+					<!-- END .organic-widgets-profile -->
 					</div>
 
-				<!-- END .organic-widgets-profile -->
+				<!-- END .organic-widgets-section -->
 				</div>
 
-			<!-- END .organic-widgets-section -->
-			</div>
+			<?php echo $args['after_widget'];
 
-		<?php } ?>
+		} //End Conditional checking for content
 
-		<?php echo $args['after_widget'];
-
-	}
+	} // End widget()
 
 	/**
 	 * Back-end widget form.
