ff930ce3c9df412d2ad74ac23bb62f07bcb0e0b3
Jesse Lee <jqlee85@gmail.com>
2017-07-10 22:40:45 -0700

    disable link when template not saved

--- admin/js/organic-widgets-editor-admin.js
+++ admin/js/organic-widgets-editor-admin.js
@@ -20,6 +20,7 @@
 		if ( ! organicWidgets.isCustomTemplate ) {
 			var customizeDisabled = 'disabled';
 			var submitDisabled = '';
+			var customizeLink = '#';
 			var updateButton = '<div id="organic-widgets-update-post" class="button button-primary button-large organic-button-large" '+submitDisabled+'>Update</div>';
 			var setPageTemplate = '<div class="organic-widgets-post-editor-update-post"><p>Please update post to apply custom template</p>'+updateButton+'<p>And then...</p></div>';
 			var buttonSize = '';
@@ -28,10 +29,11 @@
 			var setPageTemplate = '';
 			var customizeDisabled = '';
 			var submitDisabled = 'disabled';
+			var customizeLink = organicWidgets.customizeURL;
 			var buttonSize = 'organic-button-large';
 		}
 
-		var customizeButton = '<a href="'+organicWidgets.customizeURL+'" class="button button-primary button-large organic-widgets-customize-page-button '+buttonSize+'" '+customizeDisabled+'>Customize Page</a>';
+		var customizeButton = '<a href="'+customizeLink+'" class="button button-primary button-large organic-widgets-customize-page-button '+buttonSize+'" '+customizeDisabled+'>Customize Page</a>';
 		organicCustomEditDiv.attr( 'id', 'organic-widgets-post-editor' );
 		organicCustomEditDiv.addClass('postbox');
 		organicCustomEditDiv.html('<h2 class="hndle ui-sortable-handle"><img src="'+organicWidgets.leafIcon+'"/><span>Organic Custom Widgets Page</span></h2><div class="organic-widgets-post-editor-content">' + setPageTemplate + customizeButton + '</div>');
