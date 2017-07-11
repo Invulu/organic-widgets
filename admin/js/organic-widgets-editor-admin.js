8d5e9146e89f8bd8018fd3addca4840470166886
Jesse Lee <jqlee85@gmail.com>
2017-07-10 22:24:05 -0700

    Update button styling

--- admin/js/organic-widgets-editor-admin.js
+++ admin/js/organic-widgets-editor-admin.js
@@ -18,18 +18,23 @@
 		var organicCustomEditDiv = $(document.createElement('div'));
 
 		if ( ! organicWidgets.isCustomTemplate ) {
-			var updateButton = '<div id="organic-widgets-update-post" class="button button-primary button-large organic-button-large">Update</div>';
+			var customizeDisabled = 'disabled';
+			var submitDisabled = '';
+			var updateButton = '<div id="organic-widgets-update-post" class="button button-primary button-large organic-button-large" '+submitDisabled+'>Update</div>';
 			var setPageTemplate = '<div class="organic-widgets-post-editor-update-post"><p>Please update post to apply custom template</p>'+updateButton+'<p>And then...</p></div>';
 			var buttonSize = '';
+
 		} else {
 			var setPageTemplate = '';
+			var customizeDisabled = '';
+			var submitDisabled = 'disabled';
 			var buttonSize = 'organic-button-large';
 		}
 
-		var customizeButton = '<a href="'+organicWidgets.customizeURL+'" class="button button-primary button-large organic-widgets-customize-page-button '+buttonSize+'">Organic Customizer Widgets</a>';
+		var customizeButton = '<a href="'+organicWidgets.customizeURL+'" class="button button-primary button-large organic-widgets-customize-page-button '+buttonSize+'" '+customizeDisabled+'>Customize Page</a>';
 		organicCustomEditDiv.attr( 'id', 'organic-widgets-post-editor' );
 		organicCustomEditDiv.addClass('postbox');
-		organicCustomEditDiv.html('<h2 class="hndle ui-sortable-handle"><img src="'+organicWidgets.leafIcon+'"/><span>Organic Custom Page</span></h2><div class="organic-widgets-post-editor-content">' + setPageTemplate + customizeButton + '</div>');
+		organicCustomEditDiv.html('<h2 class="hndle ui-sortable-handle"><img src="'+organicWidgets.leafIcon+'"/><span>Organic Custom Widgets Page</span></h2><div class="organic-widgets-post-editor-content">' + setPageTemplate + customizeButton + '</div>');
 		wpContentEditorDiv.before(organicCustomEditDiv);
 		wpContentEditorDiv.hide();
 
